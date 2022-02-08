cac54bb50125c5327fb0102196ddedb069b97526
bluematter <michael@motionbox.io>
2022-02-08 15:32:57 -0800

    set token in cookie and add constants

---
+++ packages/videotree/src/constants.ts
@@ -0,0 +1,7 @@
+export const IS_DEV = process.env.NODE_ENV === "development" ? true : false;
+export const IS_STAGING = process.env.ENV === "staging" ? true : false;
+export const GRAPHQL_ENDPOINT = IS_DEV
+  ? "http://localhost:3001"
+  : IS_STAGING
+  ? "staging"
+  : "production";

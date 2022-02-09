b2ffb83104373c2dd22aa8ea6e0e1f235aff1cb4
bluematter <michael@motionbox.io>
2022-02-08 17:01:45 -0800

    add empty media mutation function

--- packages/prisma/api/mutation/index.ts
+++ packages/prisma/api/mutation/index.ts
@@ -1,10 +1,12 @@
 import { objectType } from "nexus";
 import auth from "./auth";
+import media from "./media";
 
 const Mutation = objectType({
   name: "Mutation",
   definition(t: any) {
     auth(t);
+    media(t);
   },
 });
 
